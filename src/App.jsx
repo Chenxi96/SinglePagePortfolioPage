70940ce2559ac5445fc4bb0095f05e2f6381f26f
Chenxi <lin.chenxi14@gmail.com>
2024-02-07 16:33:10 -0500

    added styling to site and project component

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,76 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import rings from './assets/images/pattern-rings.svg'
+import circle from './assets/images/pattern-circle.svg'
+import github from './assets/images/icon-github.svg'
+import frontEnd from './assets/images/icon-frontEnd-mentor.svg'
+import linkedin from './assets/images/icon-linkedin.svg'
+import twitter from './assets/images/icon-twitter.svg'
+import mobilePic from './assets/images/image-profile-mobile.webp'
+import Project from './projects/Project'
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img className='ringLeft' src={rings} alt="" />
+      <img className='circleRight' src={circle} alt="" />
+        <header>
+          <nav>
+            <h3>adamkeyes</h3>
+            <div className='links'>
+              <a href="https://github.com/">
+                <img src={github} alt="github-icon" />
+              </a>
+              <a href="https://www.frontendmentor.io/">
+                <img src={frontEnd} alt="frontend-icon" />
+              </a>
+              <a href="https://www.linkedin.com/">
+                <img src={linkedin} alt="linkedin-icon" />
+              </a>
+              <a href="https://twitter.com/">
+                <img src={twitter} alt="twitter-icon" />
+              </a>
+            </div>
+          </nav>
+          <img className='profileImage' src={mobilePic} alt="" width={174} />
+        </header>
+      <article className='description'>
+        <h1>Nice to meet you! I'm <span>Adam keyes.</span></h1>
+        <p>Based in the UK, I'm a front-end developer passionate about building accessible web apps that uses love.</p>
+        <button>CONTACT ME</button>
+      </article>
+      <section className='experience'>
+        <div className='experience-wrapper'>
+          <div>
+            <h2>HTML</h2>
+            <p>4 Years Experience</p>
+          </div>
+          <div>
+            <h2>CSS</h2>
+            <p>4 Years Experience</p>
+          </div>
+          <div>
+            <h2>Javascript</h2>
+            <p>4 Years Experience</p>
+          </div>
+          <div>
+            <h2>Accessibility</h2>
+            <p>4 Years Experience</p>
+          </div>
+          <div>
+            <h2>React</h2>
+            <p>3 Years Experience</p>
+          </div>
+          <div>
+            <h2>Sass</h2>
+            <p>3 Years Experience</p>
+          </div>
+        </div>
+      </section>
+      <section>
+        <Project />
+      </section>
     </>
   )
 }
---
+++ src/projects/Project.jsx
@@ -0,0 +1,86 @@
+import project1 from '../assets/images/thumbnail-project-1-small.webp'
+import project2 from '../assets/images/thumbnail-project-2-small.webp'
+import project3 from '../assets/images/thumbnail-project-3-small.webp'
+import project4 from '../assets/images/thumbnail-project-4-small.webp'
+import Project5 from '../assets/images/thumbnail-project-5-small.webp'
+import project6 from '../assets/images/thumbnail-project-6-small.webp'
+import '../projects/Project.scss'
+
+export default function Project() {
+    return(
+        <>
+            <div className='header-container'>
+                <p className="header-title">Projects</p>
+                <button className='header-btn'>CONTACT ME</button>
+            </div>
+            <div className='projects-container'>
+                <div>
+                    <img src={project1} alt="" />
+                    <div className='project-title'>
+                        <h3>Design Portfolio</h3>
+                        <p>HTML CSS</p>
+                    </div>
+                    <div className='project-btn'>
+                        <button>View Project</button>
+                        <button>View Code</button>
+                    </div>
+                </div>
+                <div>
+                    <img src={project2} alt="" />
+                    <div className='project-title'>
+                        <h3>Design Portfolio</h3>
+                        <p>HTML CSS</p>
+                    </div>
+                    <div className='project-btn'>
+                        <button>View Project</button>
+                        <button>View Code</button>
+                    </div>
+                </div>
+                <div>
+                    <img src={project3} alt="" />
+                    <div className='project-title'>
+                        <h3>Design Portfolio</h3>
+                        <p>HTML CSS</p>
+                    </div>
+                    <div className='project-btn'>
+                        <button>View Project</button>
+                        <button>View Code</button>
+                    </div>
+                </div>
+                <div>
+                    <img src={project4} alt="" />
+                    <div className='project-title'>
+                        <h3>Design Portfolio</h3>
+                        <p>HTML CSS</p>
+                    </div>
+                    <div className='project-btn'>
+                        <button>View Project</button>
+                        <button>View Code</button>
+                    </div>
+                </div>
+                <div>
+                    <img src={Project5} alt="" />
+                    <div className='project-title'>
+                        <h3>Design Portfolio</h3>
+                        <p>HTML CSS</p>
+                    </div>
+                    <div className='project-btn'>
+                        <button>View Project</button>
+                        <button>View Code</button>
+                    </div>
+                </div>
+                <div>
+                    <img src={project6} alt="" />
+                    <div className='project-title'>
+                        <h3>Design Portfolio</h3>
+                        <p>HTML CSS</p>
+                    </div>
+                    <div className='project-btn'>
+                        <button>View Project</button>
+                        <button>View Code</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
